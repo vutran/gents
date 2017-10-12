d39680f516ce9b82529b80e79dd70f929a490798
Vu Tran <vutran@dropbox.com>
2017-10-11 21:00:32 -0700

    remove lib from git

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Generates a TypeScript interface for the resource at the given endpoint
- *
- * @param {string} url - The URL to fetch
- * @param {string} interfaceName - The interface name for this resource
- * @return {Promise<string>} - A Promise that resolves the transpiled source
- */
-export default function gents(url: string, interfaceName: string): Promise<string>;
